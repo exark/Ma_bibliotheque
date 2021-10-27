4a75b74d0c7df075c79de5e98a653ea1ef0b81e0
exark <ahmedmohamedali.benhamouda@esprit.tn>
2021-10-27 22:14:21 +0200

    commit

--- app/src/main/java/com/example/mabibliotheque/MainActivity.java
+++ app/src/main/java/com/example/mabibliotheque/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.annotation.Nullable;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
@@ -19,13 +20,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
         recyclerView = findViewById(R.id.recyclerView);
         add_button = findViewById(R.id.add_button);
         add_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                Intent intent = new Intent(MainActivity.this, AddActivity.class);
+                startActivity(intent);
             }
         });
 
