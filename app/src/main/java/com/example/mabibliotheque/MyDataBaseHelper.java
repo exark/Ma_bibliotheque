cf2b2753023249cac382fcaab1d1adb166ad092b
exark <ahmedmohamedali.benhamouda@esprit.tn>
2021-10-28 01:19:43 +0200

    commit

--- app/src/main/java/com/example/mabibliotheque/MyDataBaseHelper.java
+++ app/src/main/java/com/example/mabibliotheque/MyDataBaseHelper.java
@@ -1,10 +1,13 @@
 package com.example.mabibliotheque;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
-
+import android.widget.Toast;
 import androidx.annotation.Nullable;
+
 
 class MyDataBaseHelper extends SQLiteOpenHelper {
 
@@ -19,7 +22,7 @@ class MyDataBaseHelper extends SQLiteOpenHelper {
     private  static final String COLUMN_PAGES = "book_pages";
 
 
-    public MyDataBaseHelper(@Nullable Context context) {
+    MyDataBaseHelper(@Nullable Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
         this.context = context;
     }
@@ -27,7 +30,7 @@ class MyDataBaseHelper extends SQLiteOpenHelper {
     @Override
     public void onCreate(SQLiteDatabase db) {
         String query = "CREATE TABLE " + TABLE_NAME +
-                        " (" + COLUMN_ID + "INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                        " (" + COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
                         COLUMN_TITLE + " TEXT, " +
                         COLUMN_AUTHOR + " TEXT, " +
                         COLUMN_PAGES + " INTEGER);";
@@ -38,5 +41,31 @@ class MyDataBaseHelper extends SQLiteOpenHelper {
     public void onUpgrade(SQLiteDatabase db, int i, int i1) {
         db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
         onCreate(db);
+    }
+
+    void addBook(String title, String author, int pages){
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues cv = new ContentValues();
+
+        cv.put(COLUMN_TITLE, title);
+        cv.put(COLUMN_AUTHOR, author);
+        cv.put(COLUMN_PAGES, pages);
+        long result = db.insert(TABLE_NAME,null, cv);
+        if(result == -1){
+            Toast.makeText(context, "Failed", Toast.LENGTH_SHORT).show();
+        }else {
+            Toast.makeText(context, "Added Successfully!", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    Cursor readAllData(){
+        String query="SELECT * FROM " + TABLE_NAME;
+        SQLiteDatabase db = this.getReadableDatabase();
+
+        Cursor cursor = null;
+        if(db !=null){
+            cursor = db.rawQuery(query, null);
+        }
+        return cursor;
     }
 }
